6437aeabc59a60a613a516d3d683ee83531a3d98
almak75 <111127658+almak75@users.noreply.github.com>
2023-03-23 16:05:37 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,14 +1,18 @@
-import win32api, win32con
+#import win32api, win32con
 import time
 import streamlit as st
+import pyautogui
+
 st.write('Просто оставьте эту страниццу открытой')
 st.write('Битрикс будет думать, что Вы работаете')
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
 while True:
-    click(10,10)
+    #click(10,10)
+    pyautogui.click(10, 10)
     time.sleep(60)
+    pyautogui.click(10, 10)
+    
     click(11,10)
     time.sleep(60)
+
+    
