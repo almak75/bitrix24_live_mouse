4ebc17ca6b5264b4cb8720cb0bc88ece6c863b8f
almak75 <111127658+almak75@users.noreply.github.com>
2023-03-23 15:26:20 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,13 @@
+import win32api, win32con
+import time
+st.write('Просто оставьте эту страниццу открытой')
+st.write('Битрикс будет думать, что Вы работаете')
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+while True:
+    click(10,10)
+    time.sleep(60)
+    click(11,10)
+    time.sleep(60)
